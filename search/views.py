624307d2a1091028100879d1ea6bb27199fb9a4e
pretzelogical <yingyangyo266@gmail.com>
2024-07-31 13:15:11 -0500

    Add organization search

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from common.models import Politician
+from common.models import Politician, Organization
+from django.http import HttpResponseBadRequest
 
 # Create your views here.
 
@@ -8,17 +9,34 @@ def search(request):
     if request.htmx:
         print(request.GET.get('q', ''))
         query = request.GET.get('q', '')
-        poli = Politician.objects.filter(name__icontains=query)
-        print(poli[0].name, poli[0].organizations.all())
+        category = request.GET.get('category', 'politician')
 
-        context = {
-            'search_results': {
-                'politician': poli,
+        if category == 'politician':
+            poli = Politician.objects.filter(name__icontains=query)
+            context = {
+                'search_results': {
+                    'politician': poli,
+                }
             }
-        }
-        return render(
-            request,
-            'search/components/search_results.html.jinja2',
-            context
-        )
+            return render(
+                request,
+                'search/components/search_results.html.jinja2',
+                context
+            )
+
+        if category == 'organization':
+            org = Organization.objects.filter(name__icontains=query)
+            context = {
+                'search_results': {
+                    'organization': org,
+                }
+            }
+            return render(
+                request,
+                'search/components/search_results.html.jinja2',
+                context
+            )
+
+        # Invalid category
+        return HttpResponseBadRequest('Invalid Category')
     return render(request, 'search/search.html.jinja2')
