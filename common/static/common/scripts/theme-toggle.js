bc5f272afc4b0aa3a772d7b1c08b51917a5a2aef
pretzelogical <yingyangyo266@gmail.com>
2024-08-08 14:39:01 -0500

    replace broken lightbulb

--- common/static/common/scripts/theme-toggle.js
+++ common/static/common/scripts/theme-toggle.js
@@ -1,21 +1,21 @@
 // theme-toggle.js
 const themeToggleBtn = document.getElementById('theme-toggle');
-const darkIcon = document.getElementById('theme-toggle-dark-icon');
-const lightIcon = document.getElementById('theme-toggle-light-icon');
+// const darkIcon = document.getElementById('theme-toggle-dark-icon');
+// const lightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Check for saved user preference, if any, on load of the website
 if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark');
-  darkIcon.classList.remove('hidden');
+  // darkIcon.classList.remove('hidden');
 } else {
   document.documentElement.classList.remove('dark');
-  lightIcon.classList.remove('hidden');
+  // lightIcon.classList.remove('hidden');
 }
 
 themeToggleBtn.addEventListener('click', () => {
   // Toggle icons inside button
-  darkIcon.classList.toggle('hidden');
-  lightIcon.classList.toggle('hidden');
+  // darkIcon.classList.toggle('hidden');
+  // lightIcon.classList.toggle('hidden');
 
   // If set via local storage previously
   if (localStorage.getItem('theme')) {
